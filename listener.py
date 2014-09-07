830a99438a58da8118dbb241bac0dace26a12b70
Ketil Karstad <ketil.karstad@gmail.com>
2014-09-07 17:41:22 +0200

    debugging

--- listener.py
+++ listener.py
@@ -4,6 +4,7 @@ import pickle
 class Listener(object):
   def __init__(self, port=0):
     self.socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+    self.socket.settimeout(0.1)
     self.socket.bind(('', port))
     self.address = self.socket.getsockname()
     self.sender = None
@@ -12,7 +13,11 @@ class Listener(object):
     self.socket.close()
   
   def listen(self):
-    message, self.sender = self.socket.recvfrom(4096)
+    try:
+      message, self.sender = self.socket.recvfrom(4096)
+    except socket.timeout:
+      message = None
+    return message
   
   def reply(self, message):
     self.socket.sendto(pickle.dumps(message), self.sender)
